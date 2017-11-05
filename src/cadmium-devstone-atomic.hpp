4480d0b2bfbe0098111a6872aad3ff773e85dcb1
Damian Vicino <sdavtaker@users.noreply.github.com>
2017-11-05 12:24:12 -0500

    generator of LI models for Cadmium

---
+++ src/cadmium-devstone-atomic.hpp
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2017, Damian Vicino
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CADMIUM_DEVSTONE_ATOMIC_HPP
+#define CADMIUM_DEVSTONE_ATOMIC_HPP
+
+#include<cadmium/modeling/ports.hpp>
+#include<cadmium/modeling/message_bag.hpp>
+#include<limits>
+
+extern "C" {
+int dhrystoneRun (int Number_Of_Runs);
+}
+
+
+/**
+ * @brief PDEVStone Atomic Model.
+ *
+ * PDEVStone (InternalTime, ExternalTime, Period):
+ * This model executes:
+ * - a Dhrystone for InternalCycles on each Internal transition,
+ * - a Dhrystone for ExternalCycles on each External transition,
+ * - the time advance after each external transition is Period.
+*/
+
+//  an integer input and output port for the model
+struct devstone_atomic_defs{
+    //custom ports
+    struct in : public cadmium::in_port<int> {};
+    struct out : public cadmium::out_port<int> {};
+};
+
+
+template<typename TIME>
+class devstone_atomic {
+    using defs=devstone_atomic_defs;
+public:
+    // default constructor
+    constexpr devstone_atomic() noexcept {
+        //preparing the output bag, since we return always same message
+        cadmium::get_messages<typename defs::out>(outbag).emplace_back(1);
+    }
+
+    // state definition
+    using queued_processes=int; //for readability
+    using state_type=queued_processes;
+    state_type state = 0;
+
+    // ports definition
+    using input_ports=std::tuple<typename defs::in>;
+    using output_ports=std::tuple<typename defs::out>;
+
+protected:
+    /*
+     * This model executes:
+     * - a Dhrystone for InternalCycles on each Internal transition,
+     * - a Dhrystone for ExternalCycles on each External transition,
+     * - the time advance after each external transition is Period.
+     * The following 3 variables need to be overriden by the inheriting model constructor.
+     */
+    TIME period=std::numeric_limits<float>::infinity();
+    int external_cycles=-1;
+    int internal_cycles=-1;
+    using outbag_t=typename cadmium::make_message_bags<output_ports>::type;
+    outbag_t outbag;
+
+public:
+    void internal_transition() {
+        dhrystoneRun (internal_cycles);
+        state--;
+    }
+
+    void external_transition(TIME e, typename cadmium::make_message_bags<input_ports>::type mbs) {
+        dhrystoneRun (external_cycles);
+        state+= cadmium::get_messages<typename defs::in>(mbs).size();
+    }
+
+    void confluence_transition(TIME e, typename cadmium::make_message_bags<input_ports>::type mbs) {
+        internal_transition();
+        external_transition(0.0f, mbs);
+    }
+
+    outbag_t output() const {
+        return outbag;
+    }
+
+    TIME time_advance() const {
+        return (state!=0?period:std::numeric_limits<TIME>::infinity());
+    }
+};
+
+#endif // CADMIUM_DEVSTONE_ATOMIC_HPP
---
+++ src/cadmium-event-reader.hpp
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2017, Damian Vicino
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#ifndef CADMIUM_EVENT_READER_HPP
+#define CADMIUM_EVENT_READER_HPP
+
+#include<cadmium/modeling/ports.hpp>
+#include<cadmium/modeling/message_bag.hpp>
+#include<limits>
+#include<fstream>
+
+/**
+ * Events are read from "events.txt", first column is absolute time the event has to be sent,
+ * the second column tells the integer to sent in the "out" port
+ */
+
+//  an integer output port for the model
+struct devstone_event_reader_defs{
+    //custom ports
+    struct in : public cadmium::in_port<int> {};
+    struct out : public cadmium::out_port<int> {};
+};
+
+
+template<typename TIME>
+class devstone_event_reader {
+public:
+    using defs=devstone_event_reader_defs;
+
+    // state definition is ignored, the state is hold out of the system
+    using state_type=int;
+    state_type state = 0;
+    
+    // ports definition
+    using input_ports=std::tuple<>;
+    using output_ports=std::tuple<typename defs::out>;
+    using outbag_t=typename cadmium::make_message_bags<output_ports>::type;
+    outbag_t outbag;
+
+    std::ifstream is; //the stream
+    TIME last;
+    TIME next;
+    TIME prefetched_time;
+    int prefetched_message;
+    
+    // default constructor opens the stream and sets initial time
+    constexpr devstone_event_reader() noexcept {
+        is.open("events.txt");
+        if (!is.good()) throw std::runtime_error("failed to open events file: events.txt");
+        is >> prefetched_time;
+        if (is.eof()){
+             prefetched_time = std::numeric_limits<float>::infinity();
+        } else {
+            is >> prefetched_message;
+        }
+    }
+    
+    void internal_transition() {
+        last = next;
+        fetchUntilTimeAdvances();
+    }
+    
+    void external_transition(TIME e, typename cadmium::make_message_bags<input_ports>::type mbs) {
+        assert(false && "Non external input is expected in this model");
+    }
+    
+    void confluence_transition(TIME e, typename cadmium::make_message_bags<input_ports>::type mbs) {
+        assert(false && "Non external input is expected in this model");
+    }
+    
+    outbag_t output() const {
+        return outbag;
+    }
+    
+    TIME time_advance() const {
+        return (next < std::numeric_limits<float>::infinity()?next-last:next);
+    }
+    
+    
+private:
+    //helper function
+    void fetchUntilTimeAdvances() {
+        //making use of the prefetched values
+        next = prefetched_time;
+        cadmium::get_messages<typename defs::out>(outbag) = {prefetched_message};
+        TIME t;
+        int m;
+        //fetching next messages
+        is >> t;
+        if (is.eof()){
+            //if there is no more messages, set infinity as next event time
+            prefetched_time = std::numeric_limits<float>::infinity();
+        } else { //else cache the las message fetched
+            is >> m;
+            cadmium::get_messages<typename defs::out>(outbag).push_back(m);
+            //intermediary vars for casting
+            TIME t_next;
+            int m_next;
+            is >> t_next;
+            //advance until different time is fetched
+            while( t == next){
+                if (is.eof()){
+                    prefetched_time = std::numeric_limits<float>::infinity();
+                    return;
+                } else {
+                    is >> m_next;
+                    cadmium::get_messages<typename defs::out>(outbag).push_back(m_next);
+                }
+                is >> t_next;
+            }
+            //cache the last message fetched
+            if (t_next < next) throw std::exception();
+            prefetched_time = t_next;
+            prefetched_message = m_next;
+        }
+    }
+
+};
+
+
+
+#endif // CADMIUM_EVENT_READER_HPP
