fc0469cf7ffa1e018a94b19dd3618f4307314f17
Damian Vicino <sdavtaker@users.noreply.github.com>
2018-01-14 20:45:18 -0500

    Small script to generate and run the devstones

---
+++ run_devstones.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+PREFIX=generated_`date +%Y%m%d`
+EXTERNAL=100
+INTERNAL=100
+EVENTS=events.txt
+
+mkdir -p ${PREFIX}
+cp events.txt ${PREFIX}/events.txt
+
+# Here we generated models for Cadmium
+for D in `seq 2 1 10`; do
+	for W in `seq 2 1 10`; do
+		echo "Generating model W:${W} D:${D}"
+ 		Debug/cadmium-devstone   \
+                            --kind=LI    \
+                            --width=${W} \
+                            --depth=${D} \
+                            --ext-cycles=${EXTERNAL} \
+                            --int-cycles=${INTERNAL} \
+                            --event-list=${EVENTS}\
+                            --output="${PREFIX}/LI_DEVSTONE_D${D}_W${W}.cpp"
+        done
+done
+
+# Here we build the generated models and log their metrics
+for D in `seq 2 1 10`; do
+	for W in `seq 2 1 10`; do
+		echo "Building model W:${W} D:${D}"
+                /usr/bin/time -l \
+                clang++ --std=c++14 -Isimulators/cadmium/include -Isrc \
+                        "${PREFIX}/LI_DEVSTONE_D${D}_W${W}.cpp" dhry/dhry_1.o dhry/dhry_2.o \
+                        -o "${PREFIX}/LI_DEVSTONE_D${D}_W${W}"
+        done
+done
+# Here we run the generated models and log their executions
+# Here we build the generated models and log their metrics
+for D in `seq 2 1 10`; do
+        for W in `seq 2 1 10`; do
+                echo "Running model W:${W} D:${D}"
+                /usr/bin/time -l \
+                "${PREFIX}/LI_DEVSTONE_D${D}_W${W}"
+        done
+done
+
